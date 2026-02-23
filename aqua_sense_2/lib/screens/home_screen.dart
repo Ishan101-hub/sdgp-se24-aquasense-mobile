9ea43d3ca84abc73ebd891ff4c1fba355a4929d6
Rashan Kathurusinghe <kaveeshakathurusinghe@gmail.com>
2026-02-23 14:25:46 +0530

    add reports, services, installation, plumbers, report-issue, and support screens

---
+++ aqua_sense_2/lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_bottom_nav.dart';
+import 'usage_screen.dart';
+import 'services_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  final pages = [
+    const Center(child: Text("Home", style: TextStyle(fontSize: 22))),
+    const Center(child: Text("Leakages", style: TextStyle(fontSize: 22))),
+    UsageScreen(),    // Now linked to the 'Report' tab
+    const ServicesScreen(), // Linked to the 'Service' tab
+    const Center(child: Text("Settings", style: TextStyle(fontSize: 22))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[selectedIndex],
+      bottomNavigationBar: CustomBottomNav(
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
