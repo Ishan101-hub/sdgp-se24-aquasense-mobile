dfa660c6ee3735ce43d1f71e6b1fc714fd28dfeb
Ishan Udawatte <ishan.20232686@iit.ac.lk>
2026-04-18 19:34:32 +0530

    Changing leakage logic

--- backend/main.py
+++ backend/main.py
@@ -197,8 +197,8 @@ _dev_origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "http://192.168.1.6:8000",
-    "http://192.168.1.6",
+    "http://192.168.1.7:8000",
+    "http://192.168.1.7",
 ]
 _allowed_origins = (
     _production_origins + _dev_origins if _env == "development" else _production_origins
